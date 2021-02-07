5007f27c7067c4bd8bce7c1deb615e84cf528328
Sebastian Pytel <spytel@emakina.ch>
2021-02-07 22:49:30 +0100

    init

---
+++ html/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "mysqlserver";
+$user = "root";
+$pass = "pass123";
+$db = "test_database";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
